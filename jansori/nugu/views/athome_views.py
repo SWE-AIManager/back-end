e7f9db0a8e8467e0e8ef43de0507bb094b5c8e26
Nayoung Kim <58562417+autumn-Y@users.noreply.github.com>
2022-12-05 12:58:23 +0900

    Revert "db connnect"

--- jansori/nugu/views/athome_views.py
+++ jansori/nugu/views/athome_views.py
@@ -4,44 +4,33 @@ import json
 from django.http import JsonResponse
 from datetime import datetime, time, timedelta
 
-from app.models import Recent, Goal, User
+from app.models import Recent, Goal
 
-# ex_date = datetime(2022, 11, 26) # date and time format
-# eat_time = datetime(2022, 11, 27, 12, 0) # date and time format
-# clean_date = datetime(2022, 11, 24)
-# clean_cycle = 7
-# eat_num = 2 # integer value
+ex_date = datetime(2022, 11, 26) # date and time format
+eat_time = datetime(2022, 11, 27, 12, 0) # date and time format
+clean_date = datetime(2022, 11, 24)
+clean_cycle = 7
+eat_num = 2 # integer value
 
-user_name = '허윤서'
-userId = User.objects.get(name = user_name)
-recent_data = Recent.objects.get(recent_id = userId)
-goal_data = Goal.objects.get(goal_id = userId)
+# ex_date = Recent.recent_exercise
+# eat_date = Recent.recent_meal
+# clean_date = Recent.recent_clean
+# clean_cycle = Goal.clean
+# eat_num = Recent.meal_count
 
-# error history: can't subtract offset-naive and offset-aware datetimes
-# caused by timezone inconcsistency
-ex_date = recent_data.recent_exercise.replace(tzinfo=None)
-eat_time = recent_data.recent_meal.replace(tzinfo=None)
-clean_date = recent_data.recent_clean.replace(tzinfo=None)
-
-clean_cycle = goal_data.clean
-eat_num = recent_data.meal_count
-
-now = datetime.now().replace(tzinfo=None)
-  
 def what_user_did():
     did_what = {}
-    if (now - ex_date).days:
+    if (datetime.now() - ex_date).days:
         did_what['ex_did'] = "no"
     else:
         did_what['ex_did'] = "yes"
-
-    # datetime.today().day != eat_time.day
-    if (now - eat_time).days:
+    
+    if datetime.today().day != eat_time.day:
         did_what['eat_did'] = "no"
     else:
         did_what['eat_did'] = "yes"
 
-    if (now - clean_date).days:
+    if (datetime.now() - clean_date).days:
         did_what['clean_did'] = "no"
     else:
         did_what['clean_did'] = "yes"        
@@ -49,7 +38,7 @@ def what_user_did():
     return did_what
 
 def msg_for_ex():
-    ex_no_day = int((now - ex_date).days)
+    ex_no_day = int((datetime.now() - ex_date).days)
     # if user didn't do the excercise 1, 2 days
     if ex_no_day == 1:
         hmessage = "와 어제 운동 했네! 아직 운동할 시간 있어! 화이팅 해보자구."
@@ -64,10 +53,10 @@ def msg_for_ex():
     return hmessage
 
 def msg_for_clean():
-    clean_not_day = int((now - clean_date).days)
+    clean_not_day = int((datetime.now() - clean_date).days)
 
     if clean_not_day > clean_cycle:
-        hmessage = "설정해둔 청소주기가 지났어. 청소를 안 한지 벌써" + str(clean_not_day) + "일째야. 청소기라도 살짝 밀어볼까?"
+        hmessage = "설정해둔 청소주기가 지났어. 청소를 안 한지 벌써" + str(clean_not_day) + "일이 되었어. 청소기라도 살짝 밀어볼까?"
 
     else:
         hmessage = "아직 설정한 청소주기가 돌아오지 않았어. 그래도 눈에 보이는 쓰레기가 있다면 치우는 거 잊지 말기!"
@@ -86,7 +75,8 @@ def answer_athome(request):
 
     # "ex_not_dat" is backend parameter
     # to decide whether speacker turn on the tv on purpose
-    ctx["ex_not_day"] = str((now - ex_date).days)
+    ctx["ex_not_day"] = str((datetime.now() - ex_date).days)
+    print(ctx)
     
     # count is for cover all situation
     count = 0
@@ -113,13 +103,13 @@ def answer_athome(request):
         
         else:
             if eat_num == 1:
-                ctx['hmessage'] += "오늘 한 끼만 먹었어? 든든한 한끼였으면 좋겠다. 만약에 부실한 한 끼였다면 잘 챙겨먹어! 먹고 운동하는 게 더 좋아! "
+                ctx['hmessage'] += "오늘 왜 한끼만 먹었어어. 잘 챙겨먹는 것도 엄청 중요해! "
             
             ctx['hmessage'] += msg_for_clean()
     
     # 3. when user didnt' do over 2 things
     else:
-        first_msg = "으으음 많이 놀았으니까 자신을 위해서 할 일을 해볼까? "
+        first_msg = "으으음 많이 놀았으니까 자신을 위해서 일을 해볼까? "
         ctx['hmessage'] = first_msg
 
         if "ex_did" in not_did_list:
@@ -135,13 +125,11 @@ def answer_athome(request):
 
         if "clean_did" in not_did_list:
             if eat_num == 1:
-                ctx['hmessage'] += "한 끼여도 맛있고 든든한 식사였겠지? 그래도 끼니를 거르지는 말고, 잘 챙겨먹는 거 잊지 마! "
+                ctx['hmessage'] += "오늘 왜 한끼만 먹었어어. 잘 챙겨먹는 것도 엄청 중요해! "
 
             clean_msg = msg_for_clean()
             ctx['hmessage'] = ctx['hmessage'] + clean_msg + " "
     
-    print(ctx)
-
     respon = {
         "version": "2.1",
         "resultCode": "OK",
